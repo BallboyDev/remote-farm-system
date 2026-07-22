d377d7393b89097bbd3141923affc07c6a8f2393
ballboyDev <yswgood0329@gmail.com>
2026-07-22 21:30:43 +0900

    라즈베리파이 LED 제어 코드

--- Server/gpioServer/gpioServer.py
+++ Server/gpioServer/gpioServer.py
@@ -1,9 +1,14 @@
 import socket
 import json
+import RPi.GPIO as GPIO
+import time
 
 server = socket.socket()
 server.bind(('127.0.0.1', 8888))
 server.listen()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
 
 print('Python 서버 실행 중')
 
@@ -15,6 +20,10 @@ while True:
 
     # 실제 GPIO 제어 위치
     # GPIO.output(17, GPIO.HIGH)
+    GPIO.output(17, False)
+    time.sleep(2)
+    GPIO.output(17, True)
+    time.sleep(2)
 
     client.send("OK".encode())
     client.close()
